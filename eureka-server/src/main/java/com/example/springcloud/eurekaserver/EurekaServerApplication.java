beec9fe2dd6aeedbc39b5370c5746193371fee28
clz <1814786186@qq.com>
2019-03-12 18:04:55 +0800

    user-service

---
+++ eureka-server/src/main/java/com/example/springcloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author admin
+ * @date 2019-03-12 17:15:05
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}

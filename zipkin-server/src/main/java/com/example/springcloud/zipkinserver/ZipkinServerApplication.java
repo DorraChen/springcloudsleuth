27d89221e5a64537ed7adfc229c6b8e8b95a86f2
clz <1814786186@qq.com>
2019-03-12 17:39:56 +0800

    Spring Cloud Sleuth

---
+++ zipkin-server/src/main/java/com/example/springcloud/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.springcloud.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @author admin
+ * @date 2019-03-12 17:20:05
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
